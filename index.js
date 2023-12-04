e714573198ac94befff2bce0dc54e173c0e16098
mdshamsrza <shamsraza222333@gmail.com>
2023-12-04 11:35:29 +0530

    first commit of the ignoring some files

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello------!')
 })
 app.get('/youtube',(req,res)=>{
     console.log("<h1>i'm youtube</h1>")
 })
 app.get('/login',(req,res)=>{
-  console.log("shams Raza")
+  res.send("shams Raza")
+})
+
+app.get('/code',(req,res)=>{
+    res.send("<h1>codeMS</h1>")
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
