521874341fc21b6f54248946b7ed3c7b627e9826
mdshamsrza <shamsraza222333@gmail.com>
2023-12-04 15:44:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello------!')
-})
-app.get('/youtube',(req,res)=>{
-    console.log("<h1>i'm youtube</h1>")
-})
-app.get('/login',(req,res)=>{
-  res.send("shams Raza")
+  res.send('Hello World!')
 })
 
-app.get('/code',(req,res)=>{
-    res.send("<h1>codeMS</h1>")
+app.get('/login',(req,res)=>{
+    res.send('welcome!')
 })
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
